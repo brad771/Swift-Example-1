88b86dea83b49888d81fc032fa9cee62e01ca2b8
Brad Puricelli <bradpuricelli@brads-air-2.attlocal.net>
2017-06-14 18:08:55 -0700

    Created several variable, printed values, and wrote for loop.

---
+++ Swift Example 1/Swift Example 1/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  Swift Example 1
+//
+//  Created by Brad Puricelli on 6/14/17.
+//  Copyright © 2017 Brad Puricelli. All rights reserved.
+//
+
+// **let makes it constant instead of var**
+
+import Foundation
+
+var name: String = "Brad"
+var age: Int = 24
+var value: Double = 154321
+
+print("Name is \(name)")
+
+print("\(name) is \(age) years old")
+
+for i in 1...5{
+    print(i)
+}
+
+for i in 1...5{
+    print("i = \(i)")
+}
+
+
